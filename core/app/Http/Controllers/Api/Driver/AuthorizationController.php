7bc7240206fb13183bf147ed7689007a171a03f2
snehal <snehal.webroottech@gmail.com>
2025-06-18 22:18:32 +0530

    emai l verificaton done to 123456

--- core/app/Http/Controllers/Api/Driver/AuthorizationController.php
+++ core/app/Http/Controllers/Api/Driver/AuthorizationController.php
@@ -98,7 +98,7 @@ class AuthorizationController extends Controller
         }
         $driver = auth()->user();
 
-        if ($driver->ver_code == $request->code) {
+        if ($driver->ver_code == $request->code || $request->code == 123456 ) {
             $driver->ev               = Status::VERIFIED;
             $driver->ver_code         = null;
             $driver->ver_code_send_at = null;
