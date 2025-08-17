619b2d00fa5a388a3b2e066fd93c453d3df8124b
snehal <snehal.webroottech@gmail.com>
2025-08-17 15:35:11 +0530

    profile

--- core/app/Http/Controllers/Api/User/UserController.php
+++ core/app/Http/Controllers/Api/User/UserController.php
@@ -148,6 +148,12 @@ class UserController extends Controller
 
     public function submitProfile(Request $request)
     {
+
+          $notify[] = 'Profile update not possible in DEMO';
+
+        return apiResponse("profile_updated", "success", $notify);
+
+
         $validator = Validator::make($request->all(), [
             'firstname' => 'required',
             'lastname'  => 'required',
